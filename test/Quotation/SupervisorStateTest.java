46b2a922137e1fd6a2a7ebc6cb0902ca8af941ff
unknown <b00359796@studentmail.uws.ac.uk>
2020-03-31 20:43:11 +0100

    Signed-off-by: unknown <b00359796@studentmail.uws.ac.uk>

--- test/Quotation/SupervisorStateTest.java
+++ test/Quotation/SupervisorStateTest.java
@@ -13,18 +13,22 @@ import Users.User;
 public class SupervisorStateTest {
 
 	@Test
-	public void applicantSubmitQuoteStatusChangeTest() throws IncorrectQuoteStateError {
+	public void supervisorClimbQuoteSupervisorChangeTest() throws IncorrectQuoteStateError {
 	
 		User supervisor = new Supervisor("vca", "John Smith", "broker@vca.com", "987654321", 98009, "password", 5.0, 1, true);
 		Quote usersQuote = new Quote(supervisor, "Barley", 1);
 		
         ActionFactory actionFactory = new ActionFactory();
 
-        UserAction testSubmit = actionFactory.getAction(usersQuote, supervisor, "submit");
-        testSubmit.execute("");
+        UserAction submit = actionFactory.getAction(usersQuote, supervisor, "submit");
+        submit.execute("");
         
+        UserAction testClimb = actionFactory.getAction(usersQuote, supervisor, "climb");
+        testClimb.execute("Climbed quote");
         
-        Assert.assertTrue(usersQuote.getQuoteState() instanceof SupervisorState);
+        int expected = 98009;
+        
+        Assert.assertEquals(expected, usersQuote.getSupervisor().getUserId());
 		
 	}
 
