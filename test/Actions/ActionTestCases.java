0ca5c681fea0306f0185fe1c26f64d4ffa458e80
Scott-Neary <58978385+Scott-Neary@users.noreply.github.com>
2020-03-31 15:37:25 +0100

    Update ActionTestCases.java

--- test/Actions/ActionTestCases.java
+++ test/Actions/ActionTestCases.java
@@ -14,29 +14,25 @@ import junit.framework.Assert;
 public class ActionTestCases {
 
 	@Test
-	public void testSubmitQuote() {
-		//Elements of a test case
+	public void testSubmitQuote() throws IncorrectQuoteStateError {
+		
 		User broker = new Broker("VCA", "Scott Neary", "B00381534@studentmail.uws.ac.uk", "123456789", 381534, "password", 5.0);
 		Quote quote = new Quote(broker, "Barley", 1);
 		ActionFactory myFactory = new ActionFactory();
 
-		//Test Input
 		UserAction usersubmits = myFactory.getAction(quote, broker, "submit");
 		usersubmits.execute("");
 
-		//Test Expectation
 		String expectedResult = "PENDING";
-
-		//Test Result
 		String testResult = quote.getStatus();
 
-		//Compare the result against the expectation
 		Assert.assertEquals("Quote has been submitted and is now pending.", expectedResult, testResult);
+		
 	}
 	
 	@Test
-	public void testRejectRequest() {
-		//Elements of a test case
+	public void testRejectRequest() throws IncorrectQuoteStateError {
+		
 		User supervisor = new Supervisor("VCA", "Mr. Jeffrey Jordan", "jefferyjordan@vca.com", "123456789", 381534, "password", 5.0,1, true);
 		User broker = new Broker("VCA", "Scott Neary", "B00381534@studentmail.uws.ac.uk", "123456789", 381534, "password", 5.0);
 		Quote quote = new Quote(broker, "Barley", 1);
@@ -49,19 +45,16 @@ public class ActionTestCases {
         UserAction supervisorclimbs = myFactory.getAction(quote, supervisor, "reject");
         supervisorclimbs.execute("because");
 
-        //Test Expectation
         String expectedResult = "REJECTED";
-
-        //Test Result
         String testResult = quote.getStatus();
 
-        //Compare the result against the expectation
         Assert.assertEquals("Quote has been submitted and has been rejected.", expectedResult, testResult);
+        
 	}
 
 	@Test
-	public void testApproveRequest() {
-		//Elements of a test case
+	public void testApproveRequest() throws IncorrectQuoteStateError {
+		
 		User supervisor = new Supervisor("VCA", "Mr. Jeffrey Jordan", "jefferyjordan@vca.com", "123456789", 381534, "password", 5.0, 1, true);
 		User broker = new Broker("VCA", "Scott Neary", "B00381534@studentmail.uws.ac.uk", "123456789", 381534, "password", 5.0);
 		Quote quote = new Quote(broker, "Barley", 1);
@@ -74,19 +67,17 @@ public class ActionTestCases {
         UserAction supervisorApproval = myFactory.getAction(quote, supervisor, "accept");
         supervisorApproval.execute("");
         
-        //Test Expectation
         String expectedResult = "APPROVED";
 
-        //Test Result
         String testResult = quote.getStatus();
 
-        //Compare the result against the expectation
         Assert.assertEquals("Quote has been submitted and has been approved.", expectedResult, testResult);
+        
 	}
 	
 	@Test
-	public void testClimbRejectRequest() {
-		//Elements of a test case
+	public void testClimbRejectRequest() throws WrongUserException, IncorrectQuoteStateError {
+		
 		Manager manager = new Manager("VCA", "Mr. Douglas Jackson", "douglasjackson@vca.com", "987654321", 98009, "password", 5.0);
 		User supervisor = new Supervisor("VCA", "Mr. Jeffrey Jordan", "jefferyjordan@vca.com", "123456789", 381534, "password", 5.0, 1, true);
 		User broker = new Broker("VCA", "Scott Neary", "B00381534@studentmail.uws.ac.uk", "123456789", 381534, "password", 5.0);
@@ -105,19 +96,16 @@ public class ActionTestCases {
         UserAction managerrejects = myFactory.getAction(quote, manager, "reject");
         managerrejects.execute("because");
         
-        //Test Expectation
         String expectedResult = "REJECTED";
-
-        //Test Result
         String testResult = quote.getStatus();
 
-        //Compare the result against the expectation
         Assert.assertEquals("Quote has been climbed and has been rejected.", expectedResult, testResult);
+        
 	}
 	
 	@Test
-	public void testClimbAcceptRequest() {
-		//Elements of a test case
+	public void testClimbAcceptRequest() throws WrongUserException, IncorrectQuoteStateError {
+		
 		Manager manager = new Manager("VCA", "Mr. Douglas Jackson", "douglasjackson@vca.com", "987654321", 98009, "password", 5.0);
 		User supervisor = new Supervisor("VCA", "Mr. Jeffrey Jordan", "jefferyjordan@vca.com", "123456789", 381534, "password", 5.0, 1, true);
 		User broker = new Broker("VCA", "Scott Neary", "B00381534@studentmail.uws.ac.uk", "123456789", 381534, "password", 5.0);
@@ -136,19 +124,16 @@ public class ActionTestCases {
         UserAction managerrejects = myFactory.getAction(quote, manager, "accept");
         managerrejects.execute("because");
         
-        //Test Expectation
         String expectedResult = "APPROVED";
-
-        //Test Result
         String testResult = quote.getStatus();
 
-        //Compare the result against the expectation
         Assert.assertEquals("Quote has been climbed and has been approved.", expectedResult, testResult);
+        
 	}
 	
 	@Test
-	public void testQuoteAndSupervisorZoneComparison() {
-		//Elements of a test case
+	public void testQuoteAndSupervisorZoneComparison() throws IncorrectQuoteStateError {
+		
 		User supervisor = new Supervisor("VCA", "Mr. Maurice Pippen", "mauricepippen@vca.com", "2354", 390674, "password", 5.0, 2, true);
 		User broker = new Broker("VCA", "Scott Neary", "B00381534@studentmail.uws.ac.uk", "123456789", 381534, "password", 5.0);
 		Quote quote = new Quote(broker, "Barley", 3);
@@ -161,14 +146,11 @@ public class ActionTestCases {
         UserAction supervisorApproval = myFactory.getAction(quote, supervisor, "accept");
         supervisorApproval.execute("");
        
-        //Test Expectation
         boolean expectedResult = false;
-
-        //Test Result
         boolean testResult = supervisorApproval.validate();
-        
-        //Compare the result against the expectation
+
         Assert.assertEquals("Exception is thrown as quote zone does not match supervisor zone.", expectedResult, testResult);
+        
 	}
 
 }
